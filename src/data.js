6fdf0897fa7a28325cc13bc07de3791ebd6afec2
Kern David 1 (s) <david.kern1@students.fhnw.ch>
2020-04-29 18:56:24 +0200

    example how to get an item and getallrows (data.js)

--- src/data.js
+++ src/data.js
@@ -1,18 +1,25 @@
 /* load data from csv file */
 
-var data =d3v3.csv("./data/obese-worldwide-and-switzerland-final.csv")
-    .row(function (d) {
-        return {
-            numberOfRows: d.length,
-            country: d.Country,
-            year: new Date(+d.Year),
-            bmi: d.BMI,
-            gender: d.Gender,
-            age: d.Age,
-            value: +d.Value,
-            state: d.State,
-            numberOfPersons: d.NumberOfPersons,
-            deaths: +d.Deaths
-        };
-    });
+
+var data = d3.csv("data/obese-worldwide-and-switzerland-final.csv", function (d) {
+    return {
+        country: d.Country,
+        year: +d.Year,
+        bmi: d.BMI,
+        gender: d.Gender,
+        age: d.Age,
+        value: +d.Value,
+        state: d.State,
+        numberOfPersons: d.NumberOfPersons,
+        deaths: +d.Deaths
+    };
+});
+
+//zugriff auf die daten
+data.then(function (data) {
+    console.log(data[3433].country);
+    //getAllRows
+    console.log(data.length)
+});
+
 
